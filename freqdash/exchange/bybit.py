9b9d54e06570ec1c982a473bbb9b9bc240c156ed
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-09-26 21:20:23 +0100

    Update bybit.py

--- freqdash/exchange/bybit.py
+++ freqdash/exchange/bybit.py
@@ -57,7 +57,7 @@ class Bybit(Exchange):
         limit: int = 500,
     ) -> list:
         self.check_weight()
-        params = {"symbol": {f"{base}{quote}"}, "interval": interval, "limit": limit}
+        params = {"symbol": f"{base}{quote}", "interval": interval, "limit": limit}
         if start_time is not None:
             params["startTime"] = start_time
         if end_time is not None:
