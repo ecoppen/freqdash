1d39e338b196f5caaead237bb08762a23b24aad5
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-10-05 21:06:08 +0100

    Update database.py

--- freqdash/models/database.py
+++ freqdash/models/database.py
@@ -1,9 +1,12 @@
 import logging
+from decimal import Decimal
 from pathlib import Path
+from typing import Optional
 
 import sqlalchemy as db
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
 
 log = logging.getLogger(__name__)
 
@@ -20,8 +23,8 @@ class Database:
         self.Base = declarative_base()
 
     def get_table_object(self, table_name: str):
-        self.Base.metadata.reflect(self.engine)  # type: ignore
-        return self.Base.metadata.tables.get(table_name)  # type: ignore
+        self.Base.metadata.reflect(bind=self.engine)  # type: ignore
+        return self.Base.metadata.tables[table_name]  # type: ignore
 
     def get_open_trades(self):
         table_name = "trades"
@@ -37,4 +40,26 @@ class Database:
             self.session.query(table_object)
             .filter_by(ft_trade_id=trade_id, status="closed")
             .all()
+        )
+
+    def get_closed_profit(self):
+        table_name = "trades"
+        table_object = self.get_table_object(table_name)
+        return self.session.query(func.sum(table_object.c.close_profit_abs)).scalar()
+
+    def get_closed_profit_between_dates(
+        self, start_datetime: Optional[str], end_datetime: Optional[str]
+    ):
+        table_name = "trades"
+        table_object = self.get_table_object(table_name)
+
+        filters = []
+        if start_datetime is not None:
+            filters.append(table_object.c.close_date >= start_datetime)
+        if end_datetime is not None:
+            filters.append(table_object.c.close_date <= end_datetime)
+        return (
+            self.session.query(func.sum(table_object.c.close_profit_abs))
+            .filter(*filters)
+            .scalar()
         )
