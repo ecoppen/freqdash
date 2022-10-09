d80f3e8c469b9787d0fbdd2e2c5a0521339c87f5
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-10-09 16:29:37 +0100

    Update gateio.py

--- freqdash/exchange/gateio.py
+++ freqdash/exchange/gateio.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from typing import Union
 
 from freqdash.exchange.exchange import Exchange
-from freqdash.exchange.utils import Intervals, send_public_request
+from freqdash.exchange.utils import Intervals, Settle, send_public_request
 
 log = logging.getLogger(__name__)
 
@@ -16,7 +16,7 @@ class Gateio(Exchange):
     exchange = "gate.io"
     spot_api_url = "https://api.gateio.ws"
     spot_trade_url = "https://www.gate.io/trade/BASE_QUOTE"
-    futures_api_url = "https://fx-api.gateio.ws"
+    futures_api_url = "https://api.gateio.ws"
     futures_trade_url = "https://www.gate.io/futures_trade/USDT/BASE_QUOTE"
     max_weight = 1000
 
@@ -81,4 +81,80 @@ class Gateio(Exchange):
                 }
                 for candle in raw_json
             ]
+        return []
+
+    def get_futures_price(
+        self,
+        base: str,
+        quote: str,
+        settle: Settle = Settle.USDT,
+    ) -> Decimal:
+        self.check_weight()
+        params: dict = {}
+        header, raw_json = send_public_request(
+            api_url=self.spot_api_url,
+            url_path=f"/api/v4/futures/{settle}/contracts/{base}_{quote}",
+            payload=params,
+        )
+        if len(raw_json) > 0:
+            if "last_price" in [*raw_json]:
+                return Decimal(raw_json["last_price"])
+        return Decimal(-1.0)
+
+    def get_futures_prices(
+        self,
+        settle: Settle = Settle.USDT,
+    ) -> list[dict[str, Decimal]]:
+        self.check_weight()
+        params: dict = {}
+        header, raw_json = send_public_request(
+            api_url=self.spot_api_url,
+            url_path=f"/api/v4/futures/{settle}/contracts",
+            payload=params,
+        )
+        if len(raw_json) > 0:
+            return [
+                {"symbol": pair["name"], "price": Decimal(pair["last_price"])}
+                for pair in raw_json
+            ]
+        return []
+
+    def get_futures_kline(
+        self,
+        base: str,
+        quote: str,
+        interval: Intervals = Intervals.ONE_DAY,
+        start_time: Union[int, None] = None,
+        end_time: Union[int, None] = None,
+        limit: int = 500,
+        settle: Settle = Settle.USDT,
+    ) -> list:
+        self.check_weight()
+        params: dict = {
+            "contract": f"{base}_{quote}",
+            "interval": interval,
+        }
+        if start_time is not None:
+            params["from"] = start_time
+        if end_time is not None:
+            params["to"] = end_time
+
+        header, raw_json = send_public_request(
+            api_url=self.spot_api_url,
+            url_path=f"/api/v4/futures/{settle}/candlesticks",
+            payload=params,
+        )
+
+        if len(raw_json) > 0:
+            return [
+                {
+                    "timestamp": int(candle["t"]),
+                    "open": Decimal(candle["o"]),
+                    "high": Decimal(candle["h"]),
+                    "low": Decimal(candle["l"]),
+                    "close": Decimal(candle["c"]),
+                    "volume": Decimal(candle["v"]),
+                }
+                for candle in raw_json
+            ][:limit]
         return []
