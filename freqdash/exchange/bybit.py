46b20e026327f5ad5594f438326db6ab00ec2e0f
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-10-11 17:25:11 +0100

    Update bybit.py
    close #7
    close #18

--- freqdash/exchange/bybit.py
+++ freqdash/exchange/bybit.py
@@ -85,3 +85,82 @@ class Bybit(Exchange):
                         for candle in raw_json["result"]["list"]
                     ]
         return []
+
+    def get_futures_price(self, base: str, quote: str) -> Decimal:
+        self.check_weight()
+        params = {"symbol": f"{base}{quote}"}
+        header, raw_json = send_public_request(
+            api_url=self.futures_api_url,
+            url_path="/v2/public/tickers",
+            payload=params,
+        )
+        if "result" in [*raw_json]:
+            if len(raw_json["result"]) > 0:
+                if "last_price" in [*raw_json["result"][0]]:
+                    return Decimal(raw_json["result"][0]["last_price"])
+        return Decimal(-1.0)
+
+    def get_futures_prices(self) -> list:
+        self.check_weight()
+        params: dict = {}
+        header, raw_json = send_public_request(
+            api_url=self.futures_api_url,
+            url_path="/v2/public/tickers",
+            payload=params,
+        )
+        if "result" in [*raw_json]:
+            return [
+                {"symbol": pair["symbol"], "price": Decimal(pair["last_price"])}
+                for pair in raw_json["result"]
+            ]
+        return []
+
+    def get_futures_kline(
+        self,
+        base: str,
+        quote: str,
+        start_time: int,
+        interval: Intervals = Intervals.ONE_DAY,
+        end_time: Union[int, None] = None,
+        limit: int = 500,
+    ) -> list:
+        self.check_weight()
+        custom_intervals = {
+            "1m": 1,
+            "5m": 5,
+            "15m": 15,
+            "1h": 60,
+            "4h": 240,
+            "1d": "D",
+            "1w": "W",
+        }
+        params = {
+            "symbol": f"{base}{quote}",
+            "interval": custom_intervals[interval],
+            "limit": limit,
+            "from": start_time,
+        }
+
+        if end_time is not None:
+            params["endTime"] = end_time
+
+        header, raw_json = send_public_request(
+            api_url=self.futures_api_url,
+            url_path="/public/linear/kline",
+            payload=params,
+        )
+
+        if "result" in [*raw_json]:
+            if len(raw_json["result"]) > 0:
+                return [
+                    {
+                        "timestamp": int(candle["open_time"]),
+                        "open": Decimal(candle["open"]),
+                        "high": Decimal(candle["high"]),
+                        "low": Decimal(candle["low"]),
+                        "close": Decimal(candle["close"]),
+                        "volume": Decimal(candle["volume"]),
+                    }
+                    for candle in raw_json["result"]
+                ]
+        return []
