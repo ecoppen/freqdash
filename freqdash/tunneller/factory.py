ce6fc1e1d1c0be8533a50cdcb0a9061c74b01dca
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-11-03 21:20:55 +0000

    Update factory.py

--- freqdash/tunneller/factory.py
+++ freqdash/tunneller/factory.py
@@ -1,14 +1,8 @@
 import logging
-from pathlib import Path
 
-from freqdash.core.config import load_config
-from freqdash.core.utils import send_public_request
 from freqdash.tunneller.tunnel import Tunnel
 
 log = logging.getLogger(__name__)
-
-config_path = Path(Path().resolve(), "config", "config.json")
-config = load_config(config_path)
 
 
 def load_tunnels(config: list) -> list:
