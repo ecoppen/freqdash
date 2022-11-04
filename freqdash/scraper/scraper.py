6c2457dd2cabb232d9f49ee07368711173ec2b71
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-11-04 16:44:00 +0000

    Update scraper.py

--- freqdash/scraper/scraper.py
+++ freqdash/scraper/scraper.py
@@ -25,11 +25,50 @@ class Scraper:
     def scrape_cycle(self):
         for tunnel in self.tunnels:
             tunnel.start()
-            basepath = f"http://127.0.0.1:{tunnel.local_bind_port}/api/v1/"
-            headers, status = send_public_request(
-                url=basepath + "health",
-                method="GET",
-                auth=(tunnel.api_username, tunnel.api_password),
-            )
-            log.info(status)
+            response = self.get_config(tunnel=tunnel)
+            if response:
+                log.info(f"Scraped {response['host']}")
+                result = self.database.check_then_add_or_update_host(response)
+                log.info(result)
+            response = self.get_sysinfo(tunnel=tunnel)
+            log.info(response)
             tunnel.stop()
+
+    def get_config(self, tunnel) -> dict:
+        basepath = f"http://{tunnel.remote_host}:{tunnel.local_bind_port}/api/v1/"
+        headers, json = send_public_request(
+            url=basepath + "show_config",
+            method="GET",
+            auth=(tunnel.api_username, tunnel.api_password),
+        )
+        data = {}
+        if "version" in [*json]:
+            data = {
+                "host": f"{tunnel.ssh_host}:{tunnel.ssh_port}",
+                "remote_host": f"{tunnel.remote_host}:{tunnel.remote_port}",
+                "exchange": json["exchange"],
+                "strategy": json["strategy"],
+                "state": json["state"],
+                "stake_currency": json["stake_currency"],
+                "trading_mode": json["trading_mode"],
+                "run_mode": json["runmode"],
+                "ft_version": json["version"],
+                "strategy_version": json["strategy_version"],
+            }
+
+        return data
+
+    def get_sysinfo(self, tunnel):
+        basepath = f"http://{tunnel.remote_host}:{tunnel.local_bind_port}/api/v1/"
+        headers, json = send_public_request(
+            url=basepath + "sysinfo",
+            method="GET",
+            auth=(tunnel.api_username, tunnel.api_password),
+        )
+        data = {}
+        if "cpu_pct" in [*json]:
+            data = {
+                "cpu_pct": ",".join(str(each) for each in json["cpu_pct"]),
+                "ram_pct": json["ram_pct"],
+            }
+        return data
