8dea737f3ead46576d7942a61ef43db35be12b77
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-11-21 21:35:21 +0000

    Update config.py

--- freqdash/core/config.py
+++ freqdash/core/config.py
@@ -1,21 +1,44 @@
 import json
 
-from pydantic import BaseModel, Extra, Field, IPvAnyAddress, ValidationError, validator
+from pydantic import (
+    BaseModel,
+    Field,
+    IPvAnyAddress,
+    ValidationError,
+    root_validator,
+    validator,
+)
 
 
-class FreqtradeAPI(BaseModel):
+class LocalFreqtradeAPI(BaseModel):
+    local_host: IPvAnyAddress = IPvAnyAddress.validate("127.0.0.1")
+    local_port: int = Field(5432, ge=1, le=65535)
+    api_username: str
+    api_password: str
+
+
+class RemoteFreqtradeAPI(BaseModel):
     ssh_host: IPvAnyAddress  # type: ignore
     ssh_port: int = Field(5432, ge=1, le=65535)
-    ssh_username: str
+    ssh_username: str | None
+    ssh_pkey_filename: str | None
     ssh_password: str
     remote_host: IPvAnyAddress = IPvAnyAddress.validate("127.0.0.1")
     remote_port: int = Field(5432, ge=1, le=65535)
     api_username: str
     api_password: str
 
+    @root_validator
+    def check_username_or_pkey(cls, values):
+        username, pkey = values.get("ssh_username"), values.get("ssh_pkey_filename")
+        if username is None and pkey is None:
+            raise ValueError("Username or pkey must be provided")
+        return values
+
 
 class Config(BaseModel):
-    freqtrade_instances: list[FreqtradeAPI]
+    local_freqtrade_instances: list[LocalFreqtradeAPI] | None
+    remote_freqtrade_instances: list[RemoteFreqtradeAPI] | None
     scrape_interval: int = 600
     database_name: str = "freqdash"
 
@@ -24,6 +47,15 @@ class Config(BaseModel):
         if v < 300:
             raise ValueError("Scraping interval lower limit is 300 (5 minutes)")
         return v
+
+    @root_validator
+    def check_instance_exists(cls, values):
+        local, remote = values.get("local_freqtrade_instances"), values.get(
+            "remote_freqtrade_instances"
+        )
+        if local is None and remote is None:
+            raise ValueError("No freqtrade instance found either for remote or local")
+        return values
 
 
 def load_config(path):
