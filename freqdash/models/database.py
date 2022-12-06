16a1d40be4db9e069d576ae41566d622bb357a4c
ecoppen <51025241+ecoppen@users.noreply.github.com>
2022-12-06 07:43:42 +0000

    Update database.py

--- freqdash/models/database.py
+++ freqdash/models/database.py
@@ -68,9 +68,13 @@ class Database:
             host_id = db.Column(db.Integer, primary_key=True)
             trade_id = db.Column(db.Integer, primary_key=True)
             pair = db.Column(db.String)
+            base_currency = db.Column(db.String)
+            quote_currency = db.Column(db.String)
+
             is_open = db.Column(db.Boolean)
             amount = db.Column(db.Numeric)
             stake_amount = db.Column(db.Numeric)
+            profit_abs = db.Column(db.Numeric)
             enter_tag = db.Column(db.String)
 
             fee_open_cost = db.Column(db.Numeric)
@@ -82,6 +86,7 @@ class Database:
             open_rate = db.Column(db.Numeric)
             close_timestamp = db.Column(db.Integer)
             close_rate = db.Column(db.Numeric)
+
             exit_reason = db.Column(db.String)
             stop_loss_abs = db.Column(db.Numeric)
             leverage = db.Column(db.Numeric)
@@ -98,11 +103,11 @@ class Database:
 
             amount = db.Column(db.Numeric)
             filled = db.Column(db.Numeric)
-            order_side = db.Column(db.String)
+            ft_order_side = db.Column(db.String)
             order_type = db.Column(db.String)
             order_timestamp = db.Column(db.Integer)
-            filled_timestamp = db.Column(db.Integer)
-            is_entry = db.Column(db.Boolean)
+            order_filled_timestamp = db.Column(db.Integer)
+            ft_is_entry = db.Column(db.Boolean)
             status = db.Column(db.String)
             average = db.Column(db.Numeric)
 
@@ -114,8 +119,7 @@ class Database:
         return self.Base.metadata.tables[table_name]  # type: ignore
 
     def get_all_hosts(self):
-        table_name = "hosts"
-        table_object = self.get_table_object(table_name)
+        table_object = self.get_table_object(table_name="hosts")
         return self.session.query(table_object).all()
 
     def check_then_add_or_update_host(self, data):
@@ -123,17 +127,22 @@ class Database:
         check = (
             self.session.query(table_object)
             .filter_by(host=data["host"], remote_host=data["remote_host"])
-            .all()
+            .first()
         )
-        if len(check) == 0:
+        if check is None:
             log.info(
                 f"Adding host/remote host to db. Host: {data['host']} Remote host: {data['remote_host']}"
             )
-            result = self.engine.execute(table_object.insert().values(data))
+            self.engine.execute(table_object.insert().values(data))
             log.info(
                 f"Host saved. Host: {data['host']} Remote host: {data['remote_host']}"
             )
-            return result
+            check = (
+                self.session.query(table_object)
+                .filter_by(host=data["host"], remote_host=data["remote_host"])
+                .first()
+            )
+            return check[0]
         else:
             log.info(
                 f"Updating host in db. Host: {data['host']} Remote host: {data['remote_host']}"
@@ -146,17 +155,20 @@ class Database:
             log.info(
                 f"Host updated. Host: {data['host']} Remote host: {data['remote_host']}"
             )
-        return check
+            check = (
+                self.session.query(table_object)
+                .filter_by(host=data["host"], remote_host=data["remote_host"])
+                .first()
+            )
+        return check[0]
 
     def get_open_trades(self):
-        table_name = "trades"
-        table_object = self.get_table_object(table_name)
+        table_object = self.get_table_object(table_name="trades")
 
         return self.session.query(table_object).filter_by(is_open=1).all()
 
     def get_closed_orders_for_trade(self, trade_id):
-        table_name = "orders"
-        table_object = self.get_table_object(table_name)
+        table_object = self.get_table_object(table_name="orders")
 
         return (
             self.session.query(table_object)
@@ -165,8 +177,7 @@ class Database:
         )
 
     def get_closed_profit(self):
-        table_name = "trades"
-        table_object = self.get_table_object(table_name)
+        table_object = self.get_table_object(table_name="trades")
         result = self.session.query(func.sum(table_object.c.close_profit_abs)).scalar()
         if result is None:
             return 0.0
@@ -176,8 +187,7 @@ class Database:
     def get_closed_profit_between_dates(
         self, start_datetime: Optional[str], end_datetime: Optional[str]
     ):
-        table_name = "trades"
-        table_object = self.get_table_object(table_name)
+        table_object = self.get_table_object(table_name="trades")
 
         filters = []
         if start_datetime is not None:
@@ -193,3 +203,71 @@ class Database:
             return 0.0
         else:
             return result
+
+    def add_sysinfo(self, data):
+        table_object = self.get_table_object(table_name="sysinfo")
+
+        log.info(
+            f"Adding sysinfo. Host_id: {data['host_id']} CPU: {data['cpu_pct']} RAM: {data['ram_pct']}"
+        )
+        self.engine.execute(table_object.insert().values(data))
+
+    def get_last_trade_id(self, host_id: int):
+        table_object = self.get_table_object(table_name="trades")
+        result = (
+            self.session.query(table_object)
+            .filter_by(host_id=host_id)
+            .order_by(table_object.c.trade_id.desc())
+            .first()
+        )
+        if result is None:
+            return 0
+        else:
+            return result[1]
+
+    def check_then_add_trades(self, data: dict, host_id: int):
+        table_object = self.get_table_object(table_name="trades")
+        table_keys = table_object.columns.keys()
+        for trade in data:
+            trade = trade | {"host_id": host_id}
+            check = (
+                self.session.query(table_object)
+                .filter_by(host_id=host_id, trade_id=trade["trade_id"])
+                .first()
+            )
+            if check is None:
+                adjusted_trade = {}
+                for key in table_keys:
+                    adjusted_trade[key] = trade[key]
+                log.info(
+                    f"Adding trade to db. Host: {host_id} Trade: {trade['trade_id']} - {trade['pair']}"
+                )
+                self.engine.execute(table_object.insert().values(adjusted_trade))
+                self.check_then_add_orders(
+                    data=trade["orders"], host_id=host_id, trade_id=trade["trade_id"]
+                )
+            else:
+                log.info(f"Trade {data['trade_id']} already in DB")
+
+    def check_then_add_orders(self, data: dict, host_id: int, trade_id: int):
+        table_object = self.get_table_object(table_name="orders")
+        table_keys = table_object.columns.keys()
+        for order in data:
+            order = order | {"host_id": host_id, "trade_id": trade_id}
+            check = (
+                self.session.query(table_object)
+                .filter_by(
+                    host_id=host_id, trade_id=trade_id, order_id=order["order_id"]
+                )
+                .first()
+            )
+            if check is None:
+                adjusted_order = {}
+                for key in table_keys:
+                    adjusted_order[key] = order[key]
+                log.info(
+                    f"Adding order to db. Host: {host_id} Trade: {trade_id} Order:{order['order_id']}"
+                )
+                self.engine.execute(table_object.insert().values(adjusted_order))
+            else:
+                log.info(f"Order {data['order_id']} already in DB")
