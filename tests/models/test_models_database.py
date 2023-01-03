506e6e3d4f26a1b4bca39cce31cb3d139dd7d444
ecoppen <51025241+ecoppen@users.noreply.github.com>
2023-01-03 16:27:01 +0000

    rename test class

--- tests/models/test_models_database.py
+++ tests/models/test_models_database.py
@@ -4,12 +4,18 @@ from pathlib import Path
 from freqdash.models.database import Database
 
 
-class TestCoreConfig(unittest.TestCase):
+class TestDatabase(unittest.TestCase):
     def test_database(self):
         path = Path(":memory:")
         database = Database(path=path)
 
         assert database.get_hosts_and_modes() == {}
+        assert database.get_all_hosts() == {
+            "live": {},
+            "dry": {},
+            "recent": [],
+            "open": [],
+        }
 
 
 if __name__ == "__main__":
