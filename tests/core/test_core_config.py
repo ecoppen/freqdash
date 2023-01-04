20728aece71dd9aedfa0bb55038975ebc81c2353
ecoppen <51025241+ecoppen@users.noreply.github.com>
2023-01-04 20:05:40 +0000

    Test load_config

--- tests/core/test_core_config.py
+++ tests/core/test_core_config.py
@@ -1,4 +1,7 @@
 import unittest
+from pathlib import Path
+from unittest import mock
+from unittest.mock import patch
 
 from pydantic import IPvAnyAddress, ValidationError
 
@@ -252,6 +255,33 @@ class TestCoreConfig(unittest.TestCase):
                 scrape_interval=300,
                 database_name="freqdash",
             )
+
+    @patch("builtins.open")
+    @patch("pathlib.Path.is_file")
+    def test_load_config(self, mock_is_file, mock_open):
+        path = Path("testfile.json")
+
+        mock_is_file.return_value = False
+        with self.assertRaises(ValueError) as cm:
+            load_config(path)
+            assert "testfile.json does not exist" == str(cm.exception)
+
+        mock_is_file.return_value = True
+        opener = mock.mock_open(read_data="")
+        mock_open.side_effect = opener.side_effect
+        mock_open.return_value = opener.return_value
+        with self.assertRaises(ValueError) as cm:
+            load_config(path)
+            assert "ERROR: Invalid JSON: Expecting value, line 1, column 1" == str(
+                cm.exception
+            )
+
+        opener = mock.mock_open(read_data='{"scrape_interval":0}')
+        mock_open.side_effect = opener.side_effect
+        mock_open.return_value = opener.return_value
+        with self.assertRaises(ValueError) as cm:
+            load_config(path)
+            assert "ValueError: 2 validation errors for Config" == str(cm.exception)
 
 
 if __name__ == "__main__":
