01f182afbee6573ec85727337d6215c530754580
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2023-03-02 10:33:57 +0000

    klines for futures

--- freqdash/exchange/okx.py
+++ freqdash/exchange/okx.py
@@ -57,7 +57,7 @@ class Okx(Exchange):
         interval: Intervals = Intervals.ONE_DAY,
         start_time: int | None = None,
         end_time: int | None = None,
-        limit: int = 500,
+        limit: int = 1440,
     ) -> list:
         self.check_weight()
         custom_intervals = {
@@ -132,6 +132,22 @@ class Okx(Exchange):
                 ]
         return []
 
+    def get_instance_ids(self, base: str, quote: str) -> list:
+        params = {"instType": "FUTURES"}
+        header, raw_json = send_public_request(
+            url=self.futures_api_url,
+            url_path="/api/v5/market/tickers",
+            payload=params,
+        )
+        if "data" in [*raw_json]:
+            if len(raw_json["data"]) > 0:
+                return [
+                    pair["instId"]
+                    for pair in raw_json["data"]
+                    if base in pair["instId"] and quote in pair["instId"]
+                ]
+        return []
+
     def get_futures_kline(
         self,
         base: str,
@@ -139,7 +155,48 @@ class Okx(Exchange):
         start_time: int,
         end_time: int | None = None,
         interval: Intervals = Intervals.ONE_DAY,
-        limit: int = 500,
+        limit: int = 1440,
         settle: Settle | None = None,
     ) -> list:
+        self.check_weight()
+        custom_intervals = {
+            "1m": "1m",
+            "5m": "5m",
+            "15m": "15m",
+            "1h": "1H",
+            "4h": "4H",
+            "1d": "1Dutc",
+            "1w": "1Wutc",
+        }
+        instance_ids = self.get_instance_ids(base=base, quote=quote)
+        if len(instance_ids) > 1:
+            params: dict = {
+                "instId": f"{instance_ids[1]}",
+                "bar": custom_intervals[interval],
+                "limit": limit,
+            }
+            if start_time is not None:
+                params["before"] = start_time - 1
+            if end_time is not None:
+                params["after"] = end_time + 1
+
+            header, raw_json = send_public_request(
+                url=self.futures_api_url,
+                url_path="/api/v5/market/candles",
+                payload=params,
+            )
+
+            if "data" in [*raw_json]:
+                if len(raw_json["data"]) > 0:
+                    return [
+                        {
+                            "timestamp": int(candle[0]),
+                            "open": Decimal(candle[1]),
+                            "high": Decimal(candle[2]),
+                            "low": Decimal(candle[3]),
+                            "close": Decimal(candle[4]),
+                            "volume": Decimal(candle[5]),
+                        }
+                        for candle in raw_json["data"]
+                    ]
         return []
